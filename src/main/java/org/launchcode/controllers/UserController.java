d93379607affc3c94df3886718991d3700150bbb
specialtas <specialtas@aol.com>
2023-10-25 14:04:28 -0500

    spa day studio for class 12

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,32 @@
+package org.launchcode.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if (!errors.hasErrors()) {
+            return "user/index";
+        } else {
+            return "user/add";
+        }
+
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,70 @@
+package org.launchcode.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+public class User {
+    @Size(min=5, max =15, message = "Username must be 5-15 characters long.")
+    private String username;
+    @Email(message = "Invalid email format.")
+    private String email;
+    @Size(min=6, message = "Password must be minimum 6 characters long.")
+    private String password;
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password, String verifyPassword) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    public void checkPassword(){
+        if (password != null && verifyPassword != null && !password.equals(verifyPassword)){
+           setVerifyPassword(null);
+        }
+    }
+}
+
